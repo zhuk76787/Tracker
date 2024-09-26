e72942d4231de508a005bd7cd53f3141672754c7
zhuk76787 <zhuk76787@gmail.com>
2024-09-26 13:47:26 +0400

    добавил YandexMobileMetrica

---
+++ Tracker/AnalyticsService/AnalyticEvent.swift
@@ -0,0 +1,14 @@
+//
+//  AnaliticEvent.swift
+//  Tracker
+//
+//  Created by Дмитрий Жуков on 9/26/24.
+//
+
+import Foundation
+
+enum AnaliticEvent: String {
+    case open = "Open"
+    case close = "Close"
+    case click = "Click"
+}
---
+++ Tracker/AnalyticsService/AnalyticKey.swift
@@ -0,0 +1,12 @@
+//
+//  AnalyticKey.swift
+//  Tracker
+//
+//  Created by Дмитрий Жуков on 9/26/24.
+//
+
+import Foundation
+
+enum AnalyticKey: String {
+    case key = "9564eb43-298a-412f-b507-dceaf147c3b9"
+}
