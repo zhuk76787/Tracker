8bc4f2f2305c85e7ea130d8ba7d0615b4ed2d847
zhuk76787 <zhuk76787@gmail.com>
2024-09-27 16:07:24 +0400

    добавил ScreenShot тест

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,23 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Дмитрий Жуков on 9/27/24.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+
+    func testTrackerViewControllerLight() {
+        let vc = TrackerViewController()
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+    }
+    
+    func testTrackerViewControllerDark() {
+        let vc = TrackerViewController()
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
+    }
+}
